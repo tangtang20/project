6fd190e732b4b912b0ee0019edad22b5a92d2629
小记者 <526378725@qq.com>
2021-01-29 22:39:06 +0800

    更新读取yaml文件的路径

--- case04/data_driver/yaml_driver.py
+++ case04/data_driver/yaml_driver.py
@@ -1,8 +1,10 @@
 import yaml
+import os
 
 #读取yaml文件
 def load_yaml(path):
-    file=open(path,'r',encoding='utf-8')
+    base=os.path.dirname(os.path.abspath(__file__))
+    file=open(os.path.join(base,path),'r',encoding='utf-8')
     #print(file.read())
     data=yaml.load(file,Loader=yaml.FullLoader)
     return data
